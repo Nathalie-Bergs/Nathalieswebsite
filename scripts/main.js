62125cc45d766a477128c4a22f9e1969e1d15a05
Nathalie Bergs <nathaliebergs@Nathalies-MBP.lan>
2020-06-08 16:30:35 +0200

    Heel erg bedankt voor de hulp Tim

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function mouseOver(id){
+	document.getElementById(id).style.display = "block";
+	if (id==="Experience") {
+		document.getElementById("about").style.display = "none"; 
+	}
+	else { document.getElementById("Experience").style.display = "none";
+	}
+}
+
+function mouseOut(id) {
+	
+	if (id==='') {document.getElementById("Experience").style.display = "none";
+				document.getElementById("about").style.display = "none";}
+	else {document.getElementById(id).style.display ="none"}
+}
