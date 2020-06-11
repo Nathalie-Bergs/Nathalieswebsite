5cb195641a12b5fa0987af73fe1325ab4e82f877
Nathalie Bergs <nathaliebergs@Nathalies-MacBook-Pro.local>
2020-06-11 14:38:48 +0200

    need second opinion

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ function mouseOver(id){
 	}
 	else { document.getElementById("Experience").style.display = "none";
 	}
+	if (id=="food") {
+		document.getElementById("food2").style.display = "none";
+	}
 }
 
 function mouseOut(id) {
@@ -12,4 +15,9 @@ function mouseOut(id) {
 	if (id==='') {document.getElementById("Experience").style.display = "none";
 				document.getElementById("about").style.display = "none";}
 	else {document.getElementById(id).style.display ="none"}
+}
+
+
+function mouseOn(id) {
+	document.getElementById(id).style.display = "block";
 }
