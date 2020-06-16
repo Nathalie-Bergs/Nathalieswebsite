ffd4d12112e7db5b2266a17429be2143f343acc9
Nathalie Bergs <nathaliebergs@Nathalies-MBP.lan>
2020-06-16 14:35:18 +0200

    motivation letter button added

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ function mouseOver(id){
 	document.getElementById(id).style.display = "block";
 	if (id==="Experience") {
 		document.getElementById("about").style.display = "none"; 
+		document.getElementById("motivation").style.display = "none";
 	}
-	else { document.getElementById("Experience").style.display = "none";
-	}
+	else { 
+		document.getElementById("Experience").style.display = "none";
+	}		
 }
 
 function mouseOut(id) {
